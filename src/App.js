a4af3e90d9f427ed7ec7229109724474cf2cec0d
Aditya Kulkarni <kulkarniaditya2099@gmail.com>
2021-03-26 16:03:10 +0530

    Adding Files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [cartItem, setCartItem] = useState([])
+
+    const addInCart = (item) => {
+        const isAlreadyAdded = cartItem.findIndex(function(array){
+            return array.id === item.id
+        })
+        if(isAlreadyAdded !== -1){
+            toast("Already Added In Cart", {
+                type: "error"
+            })
+        }
+        setCartItem([...cartItem, item]);
+    }
+
+    const buyNow = () => {
+        setCartItem([])
+        toast("Purchase Complete", {
+            type: "success"
+        })
+    }
+
+    const removeItem = (item) => {
+        setCartItem(cartItem.filter(i => i.id !== item.id))
+    };
+
+    return(
+        <div>
+             <h1>Hello World!</h1>
+        </div>
+    );
 }
 
 export default App;
